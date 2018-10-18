bc7d1b81b0173ef70ee5978a806561b052adfbb3
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-10-18 12:57:56 -0300

    :recycle: Notify using validation group

--- wapyce/notifier/models.py
+++ wapyce/notifier/models.py
@@ -5,19 +5,20 @@ Models of notifier app.
 from django.db import models
 from django.utils.translation import gettext as _
 
-from wapyce.validation.models import Validation
+from wapyce.validation.models import ValidationGroup
 
 # Create your models here.
 
+# pylint: disable=too-few-public-methods
 class GithubIssue(models.Model):
     """
     The GithubIssue class is a model that represents a issue of GitHub.
     """
 
-    validation_site = models.ForeignKey(
-        Validation,
+    validation_group = models.ForeignKey(
+        ValidationGroup,
         on_delete=models.PROTECT,
-        verbose_name=_('Validation')
+        verbose_name=_('Validation group')
     )
     number = models.IntegerField(verbose_name=_('Number of GitHub issue'))
     created_at = models.DateTimeField(verbose_name=_('Created at'))
