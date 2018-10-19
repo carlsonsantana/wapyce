a52c050511cba1496ca5833e6839f57a91dc96b1
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-10-19 15:30:07 -0300

    :sparkles: Support create API for Wapyce

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'social_django',
     'wapyce.core',
     'wapyce.validation',
